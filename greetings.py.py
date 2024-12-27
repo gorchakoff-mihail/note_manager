a53e63bcfdfd0054283e08204a624f164e659657
gorchakoff-mihail <gorchakoff-mihail@yandex.ru>
2024-12-27 15:01:36 +0300

    Add files via upload

---
+++ greetings.py.py
@@ -0,0 +1,13 @@
+username = 'Михаил'
+title = 'Обучение'
+content = 'Курс python-разработчик с 0 до middle'
+status = 'Активно'
+created_date = '26-12-2024'
+issue_date = '30-11-2025'
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения заметки:', issue_date)
