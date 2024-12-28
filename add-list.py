0c320066ef5649d7b1b9a81c32108ee370a554eb
gorchakoff-mihail <gorchakoff-mihail@yandex.ru>
2024-12-28 22:42:56 +0300

    Update add-list.py

--- add-list.py
+++ add-list.py
@@ -1,5 +1,16 @@
+username = 'Михаил'
 title = [
     input("Введите первый заголовок заметки: "), input("Введите второй заголовок заметки: "),
     input("Введите третий заголовок заметки: ")
 ]
-print(title)
+content = 'Курс Python-разработчик с 0 до middle'
+status = 'Активно'
+created_date = '26-12-2024'
+issue_date = '30-11-2025'
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения заметки:', issue_date)
